a71117e5f83837e391608c707208b4c98ed2a902
Rafael Possenti <rafael_possenti@hotmail.com>
2018-03-15 00:12:14 -0200

    CreateCourseDemo.java

---
+++ src/com/luv2code/springdemo/mvc/CreateCoursesDemo.java
@@ -0,0 +1,39 @@
+package com.luv2code.springdemo.mvc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.springdemo.entity.Course;
+import com.luv2code.springdemo.entity.Instructor;
+import com.luv2code.springdemo.entity.InstructorDetail;
+
+public class CreateCoursesDemo {
+	public static void main(String Args[]) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		
+		//create new session 
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start a transaction 
+			session.beginTransaction();
+			
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+		}finally {
+			//add clean up code
+			session.close();
+			factory.close();
+		}
+	}	
+}
