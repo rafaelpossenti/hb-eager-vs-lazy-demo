18ab06a987785360947f83ace7c7f6eb6f3c4bf0
Rafael Possenti <rafael_possenti@hotmail.com>
2018-03-16 20:42:58 -0200

    Eager VS lazy
    Signed-off-by: Rafael Possenti <rafael_possenti@hotmail.com>

---
+++ src/com/luv2code/springdemo/mvc/EagerLazyDemo.java
@@ -0,0 +1,49 @@
+package com.luv2code.springdemo.mvc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.springdemo.entity.Course;
+import com.luv2code.springdemo.entity.Instructor;
+import com.luv2code.springdemo.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+	public static void main(String Args[]) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		
+		//create new session 
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start a transaction 
+			session.beginTransaction();
+			
+			int theId = 1; 
+			
+			Instructor tempInstructor = session.get(Instructor.class,theId);
+			
+			System.out.println("Instructor: " + tempInstructor);
+			
+			
+			System.out.println("Courses " + tempInstructor.getCourses());
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		}finally {
+			//add clean up code
+			session.close();
+			factory.close();
+		}
+	}	
+}
